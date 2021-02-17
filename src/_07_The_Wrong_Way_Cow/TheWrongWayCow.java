0960535f335f71cc40aa6ee8e082e7c4a0c503c2
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-02-16 16:34:23 -0800

    02/16/2021
    Finished module 0!

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -50,45 +50,84 @@ package _07_The_Wrong_Way_Cow;
 
 public class TheWrongWayCow {
 
+	static int toReturn[][];
+	static int up;
+	static int down;
+	static int left;
+	static int right;
+
 	public static int[] findWrongWayCow(final char[][] field) {
 		// Fill in the code to return the [col, row] coordinate position of the
 		// head (letter 'c') of the wrong way cow!
-		int[] toReturn = new int[2];
-		int up = 0;
-		int down = 0;
-		int left = 0;
-		int right = 0;
+		left = 0;
+		right = 0;
+		up = 0;
+		down = 0;
+		toReturn = new int[4][2];
 		for (int i = 0; i < field.length; i++) {
-			String s = "HELLO";
 			for (int j = 0; j < field[i].length; j++) {
 				char currentChar = field[i][j];
-				if (j - 1 > 0) {
+				if (j - 2 >= 0) {
 					if (currentChar == 'c' && field[i][j - 1] == 'o' && field[i][j - 2] == 'w')
-						left++;
-					else if (currentChar == 'w' && field[i][j - 1] == 'o' && field[i][j - 2] == 'c')
-						right++;
-//					else if (currentChar == 'c' && field[i - 1][j] == 'o' && field[i - 2][j] == 'w')
-//						up++;
-//					else if (currentChar == 'w' && field[i - 1][j] == 'o' && field[i - 2][j] == 'c')
-//						down++;
-				} else if (j - 1 < 0) {
-					char second = field[i-1][field[i].length - 1];
-					char third = field[i-1][field[i].length - 2];
-					if (currentChar == 'c' && second == 'o' && third == 'w') left++;
-					else if (currentChar == 'w' && second == 'o' && third == 'c') right++;
-				} else {
-					char second = field[i][j-1];
-					char third = field[i-1][field[i].length - 1];
-					if (currentChar == 'c' && second == 'o' && third == 'w') left++;
-					else if (currentChar == 'w' && second == 'o' && third == 'c') right++;
+						addLeft(i, j);
+					if (currentChar == 'w' && field[i][j - 1] == 'o' && field[i][j - 2] == 'c')
+						addRight(i, j - 2);
 				}
-
-				if (left == 1 || right == 1 || up == 1 || down == 1) {
-					toReturn[0] = i;
-					toReturn[1] = j;
+				if (i - 2 >= 0) {
+					if (currentChar == 'c' && field[i - 1][j] == 'o' && field[i - 2][j] == 'w')
+						addUp(i, j);
+					if (currentChar == 'w' && field[i - 1][j] == 'o' && field[i - 2][j] == 'c')
+						addDown(i, j);
 				}
 			}
 		}
-		return toReturn;
+
+		for(int i = 0; i < toReturn.length; i++) {
+			for(int j = 0; j < toReturn[i].length; j++) { 
+				System.out.print(toReturn[i][j] + " ");
+			}
+			System.out.println();
+		}
+		if (up == 1) {
+			System.out.println("UP: " + up);
+			return toReturn[0];
+		} else if (down == 1) {
+			System.out.println("DOWN: " +down);
+			return toReturn[1];
+		} else if (left == 1) {
+			System.out.println("LEFT: "+left);
+			return toReturn[2];
+		}
+		else if (right == 1) {
+			System.out.println("RIGHT: "+right);
+			return toReturn[3];
+		} else {
+			System.out.println("Reached null");
+			return null;
+		}
+	}
+
+	static void addLeft(int i, int j) {
+		left++;
+		toReturn[2][0] = j;
+		toReturn[2][1] = i;
+	}
+
+	static void addRight(int i, int j) {
+		right++;
+		toReturn[3][0] = j;
+		toReturn[3][1] = i;
+	}
+
+	static void addUp(int i, int j) {
+		up++;
+		toReturn[0][0] = j;
+		toReturn[0][1] = i;
+	}
+
+	static void addDown(int i, int j) {
+		down++;
+		toReturn[1][0] = j;
+		toReturn[1][1] = i-2;
 	}
 }
